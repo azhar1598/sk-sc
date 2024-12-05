3ca5dcdfb6e86a747bbea2f76b5f10b3b53e4f60
azhar1598 <mohammedazhar.1598@gmail.com>
2024-12-05 18:34:37 +0530

    first

---
+++ components/common/Sidebar/sidebar.ts
@@ -0,0 +1,26 @@
+import {
+  IconApps,
+  IconUser,
+  IconSettings,
+  IconBook,
+  IconTemplate,
+  IconCurrencyDollar,
+  IconTools,
+  IconWandOff,
+  IconLogout2,
+  IconUsersGroup,
+  IconShoppingBag,
+  IconBuildingStore,
+  IconCategory2,
+  IconCreditCardPay,
+} from "@tabler/icons-react";
+
+export const sidebarItems = [
+  // { label: "Generate", icon: IconWandOff, link: "/" },
+  { label: "Merchants", icon: IconUsersGroup, link: "/merchants" },
+  { label: "Stores", icon: IconBuildingStore, link: "/stores" },
+  { label: "Products", icon: IconCategory2, link: "/products" },
+  { label: "Categories", icon: IconCreditCardPay, link: "/categories" },
+  { label: "Payments", icon: IconCreditCardPay, link: "/payments" },
+  { label: "Settings", icon: IconSettings, link: "/settings" },
+];
---
+++ lib/constants.ts
@@ -0,0 +1,269 @@
+import image1 from "@/public/assets/1.jpeg";
+import image2 from "@/public/assets/2.jpeg";
+import image3 from "@/public/assets/3.jpeg";
+
+export const indianStates = [
+  { value: "Andhra Pradesh", label: "Andhra Pradesh" },
+  { value: "Arunachal Pradesh", label: "Arunachal Pradesh" },
+  { value: "Assam", label: "Assam" },
+  { value: "Bihar", label: "Bihar" },
+  { value: "Chhattisgarh", label: "Chhattisgarh" },
+  { value: "Goa", label: "Goa" },
+  { value: "Gujarat", label: "Gujarat" },
+  { value: "Haryana", label: "Haryana" },
+  { value: "Himachal Pradesh", label: "Himachal Pradesh" },
+  { value: "Jharkhand", label: "Jharkhand" },
+  { value: "Karnataka", label: "Karnataka" },
+  { value: "Kerala", label: "Kerala" },
+  { value: "Madhya Pradesh", label: "Madhya Pradesh" },
+  { value: "Maharashtra", label: "Maharashtra" },
+  { value: "Manipur", label: "Manipur" },
+  { value: "Meghalaya", label: "Meghalaya" },
+  { value: "Mizoram", label: "Mizoram" },
+  { value: "Nagaland", label: "Nagaland" },
+  { value: "Odisha", label: "Odisha" },
+  { value: "Punjab", label: "Punjab" },
+  { value: "Rajasthan", label: "Rajasthan" },
+  { value: "Sikkim", label: "Sikkim" },
+  { value: "Tamil Nadu", label: "Tamil Nadu" },
+  { value: "Telangana", label: "Telangana" },
+  { value: "Tripura", label: "Tripura" },
+  { value: "Uttar Pradesh", label: "Uttar Pradesh" },
+  { value: "Uttarakhand", label: "Uttarakhand" },
+  { value: "West Bengal", label: "West Bengal" },
+  {
+    value: "Andaman and Nicobar Islands",
+    label: "Andaman and Nicobar Islands",
+  },
+  { value: "Chandigarh", label: "Chandigarh" },
+  {
+    value: "Dadra and Nagar Haveli and Daman and Diu",
+    label: "Dadra and Nagar Haveli and Daman and Diu",
+  },
+  { value: "Lakshadweep", label: "Lakshadweep" },
+  { value: "Delhi", label: "Delhi" },
+  { value: "Puducherry", label: "Puducherry" },
+  { value: "Ladakh", label: "Ladakh" },
+  { value: "Jammu and Kashmir", label: "Jammu and Kashmir" },
+];
+
+export const swatches = [
+  "#25262b",
+  "#868e96",
+  "#fa5252",
+  "#e64980",
+  "#be4bdb",
+  "#7950f2",
+  "#4c6ef5",
+  "#228be6",
+  "#15aabf",
+  "#12b886",
+  "#40c057",
+  "#82c91e",
+  "#fab005",
+  "#fd7e14",
+];
+
+export const storeDetail = {
+  name: "LAKNOI KABAB",
+  description:
+    "We are dedicated to providing the best services to our customers, Please contact us for online delivery and all types of party orders.",
+  tagLine: "Tastes Better",
+  address: "Nearby Nice Juice center, Opposite Mandi King, Yusuf Tekri",
+  state: "Telangana",
+  city: "Hyderabad",
+  pincode: "500008",
+  latitude: "17.396963",
+  longitude: "78.422703",
+  qrTheme: {
+    radius: "5",
+    ctaText: "To View Our Menu",
+    ctaColor: "#25262b",
+    primaryText: "Scan Here",
+    primaryColor: "#961414",
+    titleFontSize: "24",
+    secondaryColor: "#ffffff",
+  },
+  websiteTheme: {
+    titleColor: "#ffffff",
+    primaryColor: "#fa0000",
+    taglineColor: "#ededed",
+    secondaryColor: "#091151",
+    backgroundImage:
+      "https://images.pexels.com/photos/3826435/pexels-photo-3826435.jpeg",
+  },
+  businessHours: [
+    {
+      day: "Monday",
+      openTime: "17:00",
+      closeTime: "01:00",
+    },
+    {
+      day: "Tuesday",
+      openTime: "17:00",
+      closeTime: "01:00",
+    },
+    {
+      day: "Wednesday",
+      openTime: "17:00",
+      closeTime: "01:00",
+    },
+    {
+      day: "Thursday",
+      openTime: "17:00",
+      closeTime: "01:00",
+    },
+    {
+      day: "Friday",
+      openTime: "17:00",
+      closeTime: "01:00",
+    },
+    {
+      day: "Saturday",
+      openTime: "17:00",
+      closeTime: "01:00",
+    },
+    {
+      day: "Sunday",
+      openTime: "17:00",
+      closeTime: "01:00",
+    },
+  ],
+  id: "3a0f85b0-91b1-4232-aaf9-3b207ded7830",
+  createdAt: "2024-11-18T16:52:00.000Z",
+  updatedAt: "2024-11-18T17:07:24.000Z",
+  status: "ACTIVE",
+  logo: "https://storage.googleapis.com/menu-images-1/1731948720640-2821d5298f2f1796c157c1430dd1e108.png",
+  merchantDetails: {
+    name: "Mohammad Faazil Ahmad",
+    email: "faazilfaiz3612@gmail.com",
+    phoneNumber: "9143333315",
+  },
+  productCategories: [
+    {
+      id: 16,
+      name: "Kababs",
+      images: [image1.src, image2.src, image3.src],
+      products: [
+        {
+          id: 107,
+          name: "Mutton Phater Ka Gosht",
+          price: 280,
+          currency: "INR",
+        },
+        {
+          id: 108,
+          name: "Mutton Seekh Kabab",
+          price: 200,
+          currency: "INR",
+        },
+        {
+          id: 109,
+          name: "Platter",
+          price: 1899,
+          currency: "INR",
+        },
+        {
+          id: 110,
+          name: "Chicken Phater Ka Gosht",
+          price: 250,
+          currency: "INR",
+        },
+        {
+          id: 111,
+          name: "Cocktail Kabab",
+          price: 300,
+          currency: "INR",
+        },
+        {
+          id: 112,
+          name: "Barbeque Kabab",
+          price: 220,
+          currency: "INR",
+        },
+        {
+          id: 113,
+          name: "Malai Kabab",
+          price: 220,
+          currency: "INR",
+        },
+        {
+          id: 114,
+          name: "Reshmi Kabab",
+          price: 220,
+          currency: "INR",
+        },
+      ],
+    },
+    {
+      id: 17,
+      name: "Sea Food",
+      images: [
+        "https://storage.googleapis.com/menu-images-1/1731949021437-31a916522b5c57f2e9bbdd202e436c5e.jpg",
+      ],
+      products: [
+        {
+          id: 117,
+          name: "Fish Gravy",
+          price: 250,
+          currency: "INR",
+        },
+        {
+          id: 118,
+          name: "Fish Fry",
+          price: 220,
+          currency: "INR",
+        },
+        {
+          id: 119,
+          name: "Prawns Fry",
+          price: 240,
+          currency: "INR",
+        },
+      ],
+    },
+    {
+      id: 18,
+      name: "Chicken",
+      images: [
+        "https://storage.googleapis.com/menu-images-1/1731949021440-437450688f9c82f374321259e1c5cac4.jpeg",
+      ],
+      products: [
+        {
+          id: 115,
+          name: "Chicken 65 Gravy",
+          price: 220,
+          currency: "INR",
+        },
+        {
+          id: 116,
+          name: "Chicken 65",
+          price: 200,
+          currency: "INR",
+        },
+      ],
+    },
+    {
+      id: 19,
+      name: "Paratha",
+      images: [
+        "https://storage.googleapis.com/menu-images-1/1731949021440-8f474ff281017be0d2c86832de0667d7.jpg",
+      ],
+      products: [
+        {
+          id: 120,
+          name: "Laccha Paratha",
+          price: 20,
+          currency: "INR",
+        },
+      ],
+    },
+  ],
+};
+
+export const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID;
+
+export const BASE_URL =
+  process.env.NODE_ENV == "development"
+    ? process.env.NEXT_PUBLIC_API_DEV_URL
+    : process.env.NEXT_PUBLIC_API_PROD_URL;
